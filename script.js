f1720cf5a46c681b7b6c0eed81aa3a965c7906b1
Tommaso Seneci <ttoommxx@gmail.com>
2024-06-17 19:47:13 +0100

    scrolling bar first attempt

--- script.js
+++ script.js
@@ -28,39 +28,6 @@ var Cookies = new (function () {
 	};
 })();
 
-var Slideshow = new (function () {
-	this.images = [
-		["/img/slideshow/doll_and_cart.jpg", "Doll and cart"],
-		["/img/slideshow/doll_and_teddy.jpg", "Doll and teddy"],
-		["/img/slideshow/doll_christmas_tree.jpg", "Doll christmas tree"],
-		["/img/slideshow/doll_in_jumpsuit.jpg", "Doll in jumpsuit"],
-		["/img/slideshow/doll_next_to_window.jpg", "Doll next to window"],
-		["/img/slideshow/doll_with_curly_hair.jpg", "Doll with curly hair"],
-		["/img/slideshow/doll_with_fish.jpg", "Doll with fish"],
-		["/img/slideshow/doll_with_hat_and_teddy.jpg", "Doll with hat and teddy"],
-		["/img/slideshow/doll_with_hat.jpg", "Doll with hat"],
-		["/img/slideshow/doll_with_small_doll.jpg", "Doll with small doll"],
-		["/img/slideshow/doll_with_toys.jpg", "Doll with toys"],
-	];
-
-	this.index = 0;
-
-	this.set = function () {
-		let slideshow = document.getElementById("slideshow-image");
-		slideshow.style.setProperty("opacity", "0");
-		setTimeout(() => {
-			[slideshow.src, slideshow.alt] = this.images[this.index];
-			slideshow.style.setProperty("opacity", "1");
-		}, 600);
-	};
-
-	this.move = function (num) {
-		let len = this.images.length;
-		this.index = (((this.index + num) % len) + len) % len;
-		this.set();
-	};
-})();
-
 // operations at boot
 
 window.onload = function () {
@@ -69,6 +36,7 @@ window.onload = function () {
 		Cookies.set("page", "about");
 	}
 	open_page(Cookies.get("page"));
+	slideshow_fetch();
 };
 
 // functions
@@ -88,19 +56,8 @@ function open_page(page) {
 	}, 500);
 }
 
-function change_image(direction) {
-	if (direction == "right") {
-		Slideshow.move(1);
-	} else if (direction == "left") {
-		Slideshow.move(-1);
-	} else {
-		throw "Direction '" + direction + "' not recognized";
-	}
-}
-
 function open_about() {
 	open_page("about");
-	Slideshow.set();
 }
 
 function open_dolls() {
@@ -117,4 +74,28 @@ function open_accessories() {
 
 function open_cart() {
 	open_page("cart");
+}
+
+function slideshow_fetch() {
+	const images = [
+		["/img/slideshow/doll_and_cart.jpg", "Doll and cart"],
+		["/img/slideshow/doll_and_teddy.jpg", "Doll and teddy"],
+		["/img/slideshow/doll_christmas_tree.jpg", "Doll christmas tree"],
+		["/img/slideshow/doll_in_jumpsuit.jpg", "Doll in jumpsuit"],
+		["/img/slideshow/doll_next_to_window.jpg", "Doll next to window"],
+		["/img/slideshow/doll_with_curly_hair.jpg", "Doll with curly hair"],
+		["/img/slideshow/doll_with_fish.jpg", "Doll with fish"],
+		["/img/slideshow/doll_with_hat_and_teddy.jpg", "Doll with hat and teddy"],
+		["/img/slideshow/doll_with_hat.jpg", "Doll with hat"],
+		["/img/slideshow/doll_with_small_doll.jpg", "Doll with small doll"],
+		["/img/slideshow/doll_with_toys.jpg", "Doll with toys"],
+	];
+	let slideshow_div = document.getElementById("slideshow");
+	for (const [src, alt] of images) {
+		const new_image = document.createElement("img");
+		new_image.src = src;
+		new_image.alt = alt;
+		new_image.classList.add("slideshow-image");
+		slideshow_div.appendChild(new_image);
+	}
 }
